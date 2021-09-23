e8c4288877d612f02a0d4a791317aa251e504f4f
Boateng24 <56285760+Boateng24@users.noreply.github.com>
2021-09-23 14:52:32 -0700

    First commit

---
+++ whaletalk.js
@@ -0,0 +1,23 @@
+let input = 'turpentine and turtles';
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+ // console.log(input[i]);
+ for (let j = 0; j < vowels.length; j++){
+   //console.log(j);
+   if (input[i] === vowels[j]){
+   if (input[i] === 'e'){
+     resultArray.push('ee')
+   }
+    else if(input[i] === 'u'){
+      resultArray.push('uu')
+   }
+   else{
+     resultArray.push(input[i]);
+   }
+ }
+ }
+}
+
+console.log(resultArray.join('').toUpperCase());
